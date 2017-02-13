34fe5391d02fa791b1d98ca08c1e4d339f6f39f0
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-13 12:44:30 -0700

    Rendering user new form

--- spec/features/users/visitor_signs_up_spec.rb
+++ spec/features/users/visitor_signs_up_spec.rb
@@ -3,8 +3,17 @@ require "rails_helper"
 describe "a visitor signs up as a user" do
   context "they visit new_user_path" do
     it "they sign up for an account" do
+
       visit new_user_path
 
+      fill_in "user[first_name]", with: 'A'
+      fill_in "user[last_name]", with: 'S'
+      fill_in "user[email]", with: 'email@email.com'
+      fill_in "user[password]", with: 'password'
+      fill_in "user[password_confirmation]", with: 'password'
+      click_on "Create Account"
+
+      expect(page).to have_content("A S's Links")
     end
   end
 end
