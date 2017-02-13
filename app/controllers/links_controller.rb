f5fa23a4bfd4738922080a56bfdae92fb5fd05ed
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-13 14:24:51 -0700

    Link creation form without validations

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -9,4 +9,19 @@ class LinksController < ApplicationController
     end
   end
 
+  def create
+    link = current_user.links.new(link_params)
+    if link.save
+      flash[:success] = "#{link.url} saved as #{link.title}"
+    else
+      flash[:alert] = link.errors.full_messages
+    end
+    redirect_to links_path
+  end
+
+  private
+    def link_params
+      params.require(:link).permit(:title, :url)
+    end
+
 end
