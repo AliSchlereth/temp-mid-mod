da29881b35e160d696a1c071673c56c3e225932a
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-13 13:43:30 -0700

    Update nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: "links#index"
-  get '/login',   to: "sessions#new",     as: "login"
-  post '/login',  to: "sessions#create"
+  get '/login',    to: "sessions#new",     as: "login"
+  post '/login',   to: "sessions#create"
+  delete '/login', to: "sessions#destroy"
 
   resources :links, only: [:index]
   resources :users, only: [:new, :create]
