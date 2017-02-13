dfab8978f4ded4547d789de674fe06f304ca738a
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-13 14:49:13 -0700

    Add classes to Mark as Read links

--- spec/features/links/user_adds_a_link_spec.rb
+++ spec/features/links/user_adds_a_link_spec.rb
@@ -29,5 +29,16 @@ describe "user adds a link" do
 
       expect(page).to have_content('Url is not a valid URL')
     end
+    it "returns a different flash error message with missing title" do
+      user = User.create(first_name: "a", last_name: "s", email: "a@email.com", password: "password", password_confirmation: "password")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit links_path
+
+      fill_in "link[url]", with: "https://google.com"
+      click_button "Add Link"
+
+      expect(page).to have_content("Title can't be blank")
+    end 
   end
 end
