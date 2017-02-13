5dc1bc5654096b6a534af82627d075d06a856eec
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-13 12:15:44 -0700

    User validations tested

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    it "is invalid without a first_name" do
+      user = User.new( last_name: "s", email: "a@email.com", password: "password", password_confirmation: "password")
+
+      expect(user).to_not be_valid
+    end
+    it "is invalid without a last_name" do
+      user = User.new(first_name: "a", email: "a@email.com", password: "password", password_confirmation: "password")
+
+      expect(user).to_not be_valid
+    end
+    it "is invalid without a email" do
+      user = User.new(first_name: "a", last_name: "s", password: "password", password_confirmation: "password")
+
+      expect(user).to_not be_valid
+    end
+    it "is invalid without a password" do
+      user = User.new(first_name: "a", last_name: "s",  email: "a@email.com", password_confirmation: "password")
+
+      expect(user).to_not be_valid
+    end
+    it "is valid with a first_name, last_name, email, and password" do
+      user = User.create(first_name: "a", last_name: "s", email: "a@email.com", password: "password", password_confirmation: "password")
+
+      expect(user).to be_valid
+    end
+
+
+  end
+
 end
+
+# User.create(first_name: "a", last_name: "s", email: "a@email.com", password: "password", password_confirmation: "password")
