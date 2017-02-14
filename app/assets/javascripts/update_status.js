58554a912acfb50c3c3be8e8d196686d1da08c59
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-13 17:51:35 -0700

    Add update hotReads api call

--- app/assets/javascripts/update_status.js
+++ app/assets/javascripts/update_status.js
@@ -1,11 +1,7 @@
 
-
-$(document).ready(function(){
-  $('.mark-as-read').on('click',function(e){
-    e.preventDefault();
-    var linkID = $(this)[0].id.split('-')[1];
-    axios.patch('/api/v1/links/' + linkID, {
-      read: true
+function updatedLinkStatus(linkID) {
+  axios.patch('/api/v1/links/' + linkID, {
+    read: true
   })
   .then(function(response){
     console.log(response)
@@ -13,5 +9,26 @@ $(document).ready(function(){
   .catch(function(error){
     console.log(error)
   });
+}
+
+function updateHotReads(linkURL) {
+  axios.post('https://powerful-wave-14129.herokuapp.com/api/v1/links', {
+    url: linkURL
+  })
+  .then(function(response){
+    console.log(response)
+  })
+  .catch(function(error){
+    console.log(error)
+  })
+}
+
+$(document).ready(function(){
+  $('.mark-as-read').on('click',function(e){
+    e.preventDefault();
+    var linkID = $(this)[0].id.split('-')[1];
+    var linkURL = $(this).parents('.row').children('.URL').text();
+    updatedLinkStatus(linkID);
+    updateHotReads(linkURL);
   });
 })
