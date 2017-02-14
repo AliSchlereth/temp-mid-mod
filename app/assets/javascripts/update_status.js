c48a0c9611debb429318f52d1643ca0c6342a779
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-13 17:35:12 -0700

    Make axios call to internal dynamic for link id

--- app/assets/javascripts/update_status.js
+++ app/assets/javascripts/update_status.js
@@ -1,30 +1,10 @@
 
-function updateLink(e){
-  e.preventDefault();
-  var linkID = $(this)[0].id.split('-')[1];
-
-  $.ajax({
-    url: '/api/v1/links' + linkID ,
-    method: 'PATCH',
-    data: {read: true}
-  })
-  .done(updateStatusOnPage)
-  .fail(onFail);
-}
-
-function onFail(error) {
-  console.error(error)
-}
-
-function updateStatusOnPage() {
-  debugger;
-}
-
 
 $(document).ready(function(){
   $('.mark-as-read').on('click',function(e){
     e.preventDefault();
-    axios.patch('/api/v1/links/1', {
+    var linkID = $(this)[0].id.split('-')[1];
+    axios.patch('/api/v1/links/' + linkID, {
       read: true
   })
   .then(function(response){
